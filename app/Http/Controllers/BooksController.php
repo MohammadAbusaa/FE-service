e5a67c947ecb8298b5d946d2d07e979b6e82f559
MohammadAbusaa <47559635+MohammadAbusaa@users.noreply.github.com>
2021-07-23 12:46:52 +0300

    first commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp;
+use GuzzleHttp\Client;
+
+class BooksController extends Controller{
+    public function index()
+    {
+        $client=new \GuzzleHttp\Client([
+            'base_uri'=>'http://192.168.1.21:8000',
+        ]);
+        // $url='http://192.168.1.21:8000/books';
+        // $curl = curl_init($url);
+        // //curl_setopt($curl, CURLOPT_POST, true);
+        // //curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // $response = curl_exec($curl);
+        // curl_close($curl);
+
+        $response=$client->get('/books');
+
+        return view('welcome',['books'=>json_decode($response->getBody())]);
+    }
+
+    public function show(Request $request)
+    {
+
+        $data=$this->validate($request,[
+            'bookName'=>'required',
+            'method'=>'required',
+        ]);
+
+        $client=new Client();
+
+        $response=$client->get('http://192.168.1.21:8000/books/search/'.$data['method'].'/'.$data['bookName']);
+
+        return view('results',['result'=>json_decode($response->getBody())]);
+    }
+
+    public function info($id)
+    {
+        $client=new Client();
+
+        $response=$client->get('http://192.168.1.21:8000/info/'.$id);
+
+        return view('info_page',['info'=>json_decode($response->getBody())]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <table class="table table-striped">
+        <tbody>
+            <tr>
+                <th scope="row">
+                    <h1>Welcome to bazar.com! the world's smallest bookstore.</h1>
+                </th>
+                <td>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <th scope="row">
+                                    Search for a book
+                                </th>
+                                <form action="http://localhost:8000/books/search" method="get">
+                                <td>
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" name="bookName" placeholder="type something here">
+                                      </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="method" id="flexRadioDefault1" value="type">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                              by type
+                                            </label>
+                                          </div>
+                                          <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="method" id="flexRadioDefault2" value="name" checked>
+                                            <label class="form-check-label" for="flexRadioDefault2">
+                                              by name
+                                            </label>
+                                          </div>
+                                          <span class="col-12">
+                                            <button class="btn btn-primary" type="submit">Search</button>
+                                          </span>
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">
+                    Available books
+                    <table class="table table-striped table-primary table-hover table-bordered" style="margin-top: 50px;">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col"></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($books as $book)
+                            <tr>
+                                <th scope="row">{{$book->id}}</th>
+                                <td><a href="http://localhost:8000/info/{{$book->id}}">{{$book->name}}</a></td>
+                                <td>{{$book->price}}</td>
+                                @if ($book->qty)
+                                    <td>
+                                        <button class="btn btn-success">purchase</button>
+                                    </td>
+                                @else
+                                    <td class="table-danger">out of stock!</td>
+                                @endif
+                            </tr>
+                          @endforeach
+                        </tbody>
+                      </table>
+                </th>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
